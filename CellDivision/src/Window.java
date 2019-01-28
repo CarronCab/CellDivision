0b54db0eb3a5f4170859d8d03c407552aa266e64
Hevilio <Hevilio@users.noreply.github.com>
2019-01-28 10:53:42 +0100

    First

--- CellDivision/src/Window.java
+++ CellDivision/src/Window.java
@@ -1,34 +1,50 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
-import javax.swing.JFrame;
+public class Window  {
+
+    private static final Dimension PREF_SIZE = new Dimension(50, 50);
+    private JPanel ui;
+    private JPanel gameGrid;
 
 @SuppressWarnings("serial") //Ignore this warning : "The serializable class Window does not declare a static final serialVersionUID field of type long"
 
-public class Window extends JFrame{
 
-	WindowContent panel;
-	public static final int WIDTH = 1680;
-	public static final int HEIGHT = 1050;
-
+	
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	
 	public Window() {
 		super();
-		WindowParameters();
+		JFrame panel=new JFrame();
+		panel.setSize(WIDTH, HEIGHT);		//set the size of the window
+		panel.setResizable(false);	//Resizable not allowed
+		panel.setVisible(true);
+		panel.setLocationRelativeTo(null); //Create the window in the middle of the screen
+		panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Ends the program when the window is closed
+		panel.setAlwaysOnTop(true); //Allows the window to stay always on top of every opened windows
 
-
+		JLabel yellowLabel = new JLabel();
+        yellowLabel.setOpaque(true);
+        yellowLabel.setBackground(new Color(248, 213, 131));
+        yellowLabel.setPreferredSize(new Dimension(780,500 ));
+        yellowLabel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+        JPanel greenMenuBar = new JPanel();
+        greenMenuBar.setOpaque(true);
+        greenMenuBar.setBackground(new Color(154, 165, 127));
+        greenMenuBar.setPreferredSize(new Dimension(780,40));
+        greenMenuBar.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		panel.getContentPane().add(greenMenuBar,BorderLayout.SOUTH);
+        panel.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+        
+		panel.pack();
+		panel.setVisible(true);
+	}
+	public static void main(String[] args) {
+		Window window=new Window();
 
 	}
-
-	private void WindowParameters() {
-
-		this.setSize(WIDTH, HEIGHT);		//set the size of the window
-		this.setResizable(false);	//Resizable not allowed
-		this.setVisible(true);
-		this.setLocationRelativeTo(null); //Create the window in the middle of the screen
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Ends the program when the window is closed
-		this.setAlwaysOnTop(true); //Allows the window to stay always on top of every opened windows
-
-		panel = new WindowContent();
-		this.setContentPane(panel);
-
-	}
-
 }
